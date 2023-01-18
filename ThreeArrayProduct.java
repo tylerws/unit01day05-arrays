d27dd43e935cb74960f9c40765fe874a06ad0f47
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-18 11:53:52 -0600

    completed threearray

--- ThreeArrayProduct.java
+++ ThreeArrayProduct.java
@@ -1,6 +1,8 @@
 public class ThreeArrayProduct {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
         
+        int[] a = {4, 0, 3};
+        int result = (a[0] * a[1] * a[2]);
+        System.out.println("[" + a[0] + ", " + a[1] + ", " + a[2]+ "]" + " | " + result);
     }
 }
