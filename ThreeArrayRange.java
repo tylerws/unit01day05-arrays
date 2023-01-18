e250e935647fbcbe8c6ddfee59b940dff3ff43aa
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-18 12:01:51 -0600

    started setting up conditionals

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,13 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        
+        int ac = Math.abs(a[0] - a[2]);
+        int ab = Math.abs(a[0] - a[1]);
+        int bc = Math.abs(a[1] - a[2]);
+
+        if (ac > ab && ac > bc) {
+            
+        }
     }
 }
