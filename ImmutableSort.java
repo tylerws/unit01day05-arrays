bd51c68e2c41f3f63097984b0e85b58582fe79cf
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-19 12:09:40 -0600

    completed immutable sort

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,19 @@
 public class ImmutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {2, 2, 2};
         
-    }
+        if (a[0] <= a[1] && a[1] <= a[2]) {
+            System.out.println(a[0] + ", " + a[1] + ", " + a[2]);
+        } else if (a[1] <= a[0] && a[0] <= a[2]) {
+            System.out.println(a[1] + ", " + a[0] + ", " + a[2]);
+        } else if (a[2] <= a[1] && a[1] <= a[0]) {
+            System.out.println(a[2] + ", " + a[1] + ", " + a[0]);
+        } else if (a[2] <= a[0] && a[0] <= a[1]) {
+            System.out.println(a[2] + ", " + a[0] + ", " + a[1]);
+        } else if (a[1] <= a[2] && a[2] <= a[0]) {
+            System.out.println(a[1] + ", " + a[2] + ", " + a[0]);
+        } else if (a[0] <= a[2] && a[2] <= a[1]) {
+            System.out.println(a[0] + ", " + a[2] + ", " + a[1]);
+        } 
+}
 }
