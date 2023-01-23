b4270496d5c044586f602e33e0155b2f788339c0
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-23 12:12:41 -0600

    Implemented MutableSort

--- MutableSort.java
+++ MutableSort.java
@@ -1,6 +1,26 @@
 // TODO: finish this
+import java.util.Arrays;
+
 public class MutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {4, 0, 3};
+        int[] b;
+        
+        
+        if (a[0] <= a[1] && a[1] <= a[2]) {
+            b = new int[] {a[2], a[1], a[0]};
+        } else if (a[1] <= a[0] && a[0] <= a[2]) {
+            b = new int[] {a[2], a[0], a[1]};
+        } else if (a[2] <= a[1] && a[1] <= a[0]) {
+            b = new int[] {a[0], a[1], a[2]};
+        } else if (a[2] <= a[0] && a[0] <= a[1]) {
+            b = new int[] {a[1], a[0], a[2]};
+        } else if (a[1] <= a[2] && a[2] <= a[0]) {
+            b = new int[] {a[0], a[2], a[1]};
+        } else {
+            b = new int[] {a[1], a[2], a[0]};
+        } 
+        a = b;
+        System.out.println(Arrays.toString(a));
     }
 }
