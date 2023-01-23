5dd9cc10cdc4f18e0836d30e2f3a624e88c14409
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-23 12:02:10 -0600

    implement ImmutableSort

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,19 +1,25 @@
+import java.util.Arrays;
+
 public class ImmutableSort {
     public static void main(String[] args) {
-        int[] a = {3, 2, 1};
+        int[] a = {4, 0, 3};
+        int[] b;
+        
         
         if (a[0] <= a[1] && a[1] <= a[2]) {
-            System.out.println(a[0] + ", " + a[1] + ", " + a[2]);
+            b = new int[] {a[0], a[1], a[2]};
         } else if (a[1] <= a[0] && a[0] <= a[2]) {
-            System.out.println(a[1] + ", " + a[0] + ", " + a[2]);
+            b = new int[] {a[1], a[0], a[2]};
         } else if (a[2] <= a[1] && a[1] <= a[0]) {
-            System.out.println(a[2] + ", " + a[1] + ", " + a[0]);
+            b = new int[] {a[2], a[1], a[0]};
         } else if (a[2] <= a[0] && a[0] <= a[1]) {
-            System.out.println(a[2] + ", " + a[0] + ", " + a[1]);
+            b = new int[] {a[2], a[0], a[1]};
         } else if (a[1] <= a[2] && a[2] <= a[0]) {
-            System.out.println(a[1] + ", " + a[2] + ", " + a[0]);
-        } else if (a[0] <= a[2] && a[2] <= a[1]) {
-            System.out.println(a[0] + ", " + a[2] + ", " + a[1]);
+            b = new int[] {a[1], a[2], a[0]};
+        } else {
+            b = new int[] {a[0], a[2], a[1]};
         } 
+
+        System.out.println(Arrays.toString(b));
 }
 }
